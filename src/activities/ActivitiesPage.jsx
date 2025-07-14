733fdf9b5970c7ab17fa9580a643a72793505c65
karl <karl.fahlbusch7241@gmail.com>
2025-07-14 12:11:15 -0500

    working on activitites page

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,4 +1,32 @@
+import { useAuth } from "../auth/AuthContext";
+import useQuery from "../api/useQuery";
+import useMutation from "../api/useMutation";
+import { useState } from "react";
+
 export default function ActivitiesPage() {
+  const { token } = useAuth();
+  const {
+    data: activities,
+    loading,
+    error,
+  } = useQuery("/activities", "activities");
+
+  const { mutate: deleteActivity, error: deleteError } = useMutation(
+    "DELETE",
+    "",
+    ["activities"]
+  );
+
+  const { mutate: addActivity, error: addError } = useMutation(
+    "POST",
+    "/activities",
+    ["activities"]
+  );
+
+  const [newActivity, setNewActivity] = useState({
+    name: "",
+    description: "",
+  });
   return (
     <>
       <h1>Activities</h1>
